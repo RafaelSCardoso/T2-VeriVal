38683d39b9acc8eadfe9825f814af3bb706f06f4
Pércio Reinert <percioreinert@gmail.com>
2020-11-17 21:37:58 -0300

    add implementation

--- my-app/src/test/java/com/mycompany/app/RegraImpostoComprasGrandesTeste.java
+++ my-app/src/test/java/com/mycompany/app/RegraImpostoComprasGrandesTeste.java
@@ -19,16 +19,16 @@ public class RegraImpostoComprasGrandesTeste {
     public void calcula_itensDiferentes() {
         List<ItemVenda> itens = new ArrayList<>();
         itens.add(123, 456, 3, 10.0); // 1,5
-        itens.add(234, 567, 2, 20.0); // 2,0
-        itens.add(345, 678, 1, 30.0); // 3,0
-        itens.add(456, 789, 3, 15.0); // 2,25
+        itens.add(234, 567, 2, 20.0); // 4,0
+        itens.add(345, 678, 1, 30.0); // 1,5
+        itens.add(456, 789, 3, 15.0); // 4,5
         itens.add(567, 890, 2, 25.0); // 5,0
-        itens.add(678, 901, 1, 35.0); // 3,5
+        itens.add(678, 901, 1, 35.0); // 1,75
 
         RegraImposto regraImposto = new RegraImpostoComprasGrandes();
         double imposto = regraImposto.calcula(itens);
 
-        assertEquals(imposto, 17,25);
+        assertEquals(imposto, 18,25);
     }
 
     @Test
